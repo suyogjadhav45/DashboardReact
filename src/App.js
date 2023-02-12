155faf6756f2549c69c8b3208aa9a22c01fdcafb
Suyog Jadhav <suyogjadhav045@gmail.com>
2023-02-12 16:29:50 +0530

    Navbar and Sign up Button Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      heyyy
+      {/* heyyy */}
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' />
+        </Routes>
+      </Router>
     </div>
   );
 }
